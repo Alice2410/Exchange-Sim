626f80adf30a62149b23e5d8847debbf4aaed792
Alice2410 <devo4ka2410@gmail.com>
2023-03-22 17:00:00 +0300

    feat: add a button to create a new ticker

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AddTickerButton from './components/AddTickerButton/AddTickerButton';
 
 function App() {
   return (
     <div className="App">
-      <div>app</div>
+      <AddTickerButton></AddTickerButton>
     </div>
   );
 }
---
+++ src/components/AddTickerButton/AddTickerButton.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
+
+const AddTickerButton = () => {
+  return(
+    <button>
+      <AddIcon/>
+    </button>
+  )
+};
+
+export default AddTickerButton;
