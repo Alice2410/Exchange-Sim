fff6dac1656ab14722339181d7cde556daec0af0
Alice2410 <devo4ka2410@gmail.com>
2023-03-29 21:22:33 +0300

    feat: add tiker's context provider and change some styles in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 import AddTickerButton from './components/AddTickerButton/AddTickerButton';
+import Ticker from './components/Ticker/Ticker';
+import { TickerProvider } from './context/TickerContext';
 
 function App() {
   return (
-    <div className="App">
-      <AddTickerButton></AddTickerButton>
+    <div className={styles.app}>
+      <TickerProvider>
+        <AddTickerButton></AddTickerButton>
+        <Ticker></Ticker>
+      </TickerProvider>
+      
     </div>
   );
 }
