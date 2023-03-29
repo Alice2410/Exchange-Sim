cb3c8d6d75ba05afee49b45213f85d0c5a17a72b
Alice2410 <devo4ka2410@gmail.com>
2023-03-29 21:12:37 +0300

    feat: add validation for currency amount input

---
+++ src/hooks/useCurrencyAmount.tsx
@@ -0,0 +1,18 @@
+import { useCallback, useState } from "react";
+
+
+export const useCurrencyAmount = () => {
+  const [value, setValue] = useState('1');
+
+  const processAmountChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const currentValue = e.target.value.replace(/\s/g,'');
+
+    if (currentValue === '') return setValue('');
+    
+    const newValue = Number(currentValue);
+
+    if (newValue) return setValue(newValue.toLocaleString());
+  }, []);
+
+  return {value, processAmountChange};
+}
