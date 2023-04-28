4df9ce2903c0108e62b13943852462e0f9fd6a41
Alice2410 <devo4ka2410@gmail.com>
2023-04-28 20:18:05 +0300

    feat: add provider for websocket context

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { WSProvider } from './context/WSContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <WSProvider>
+      <App />
+    </WSProvider>
   </React.StrictMode>
 );
 
