43cd4c14d05091c429d01df968809099ad23d9ad
Alice2410 <devo4ka2410@gmail.com>
2023-05-07 15:00:17 +0300

    refactor: rename Instrument component

--- src/components/Ticker/Ticker.tsx
+++ src/components/Ticker/Ticker.tsx
@@ -1,7 +1,7 @@
 import { useTicker } from "../../hooks/useTicker";
 import styles from "./Ticker.module.css"
 import Buyer from "../Buyer/Buyer";
-import Instrument from "../Instrument/Instrument";
+import InstrumentSelect from "../Instrument/InstrumentSelect";
 import Seller from "../Seller/Seller";
 
 const Ticker = () => {
@@ -10,7 +10,7 @@ const Ticker = () => {
   return(
     <div className={styles.ticker} hidden={!isTickerOpened}>
       <form action="">
-        <Instrument></Instrument>
+        <InstrumentSelect></InstrumentSelect>
         <div className={styles.trader_container}>
           <Seller></Seller>
           <Buyer></Buyer>
