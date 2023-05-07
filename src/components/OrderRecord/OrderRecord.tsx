0b2daf880bb58983e0cd6d06bcbb9cdf8052986e
Alice2410 <devo4ka2410@gmail.com>
2023-05-07 14:57:42 +0300

    refactor: rename offer variable

--- src/components/OrderRecord/OrderRecord.tsx
+++ src/components/OrderRecord/OrderRecord.tsx
@@ -1,25 +1,27 @@
-import { Instrument, OrderSide, OrderStatus } from "../../Enums";
+import { OrderSide, OrderStatus } from "../../Enums";
+import { getInstrumentName } from "../../helpers/getInstrumentName";
 import { Offer } from "../../Models/ServerMessages";
 import styles from "./OrderRecord.module.css";
 
 interface OrderRecordProps {
   index: number;
-  item: Offer;
+  offer: Offer;
 }
 
 
 const OrderRecord = (props: OrderRecordProps) => {
-  let {index, item} = props;
+  let {index, offer} = props;
+
   return(
     <tr className={styles.table_row} key={index}>
       <td className={styles.table_cell}>{++index}</td>
-      <td className={styles.table_cell}>{item.lastUpdate}</td>
-      <td className={styles.table_cell}>{item.lastUpdate}</td>
-      <td className={styles.table_cell}>{OrderStatus[item.status]}</td>
-      <td className={styles.table_cell}>{OrderSide[item.side]}</td>
-      <td className={styles.table_cell}>{item.price}</td>
-      <td className={styles.table_cell}>{item.amount}</td>
-      <td className={styles.table_cell}>{Instrument[item.instrument]}</td>
+      <td className={styles.table_cell}>{offer.lastUpdate}</td>
+      <td className={styles.table_cell}>{offer.lastUpdate}</td>
+      <td className={styles.table_cell}>{OrderStatus[offer.status]}</td>
+      <td className={styles.table_cell}>{OrderSide[offer.side]}</td>
+      <td className={styles.table_cell}>{offer.price}</td>
+      <td className={styles.table_cell}>{offer.amount}</td>
+      <td className={styles.table_cell}>{getInstrumentName(offer.instrument)}</td>
     </tr>
   )
 }
